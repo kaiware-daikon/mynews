09a6e2e65f5a06b2accde4d922f2d377259158ef
EC2 Default User <ec2-user@ip-172-31-33-32.us-east-2.compute.internal>
2020-04-26 10:46:03 +0000

    投稿データの保存

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -18,29 +18,30 @@
                         </ul>
                     @endif
                     
-                    <!--課題4 氏名、性別、趣味、自己紹介欄の作成-->
+                    <!--課題4 氏名、性別、趣味、自己紹介欄の作成、課題6 バリデーションの設定-->
                     <div class="form-group row">
-                        <label class="col-md-2">氏名</label>
+                        <label class="col-md-2" for="name">氏名</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="name">{{ old('name') }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">性別</label>
+                        <label class="col-md-2" for="gender">性別</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="gender">{{ old('gender') }}</textarea>
+                            <input type="radio" name="gender" value="男性">男性{{ old('gender') }}
+                            <input type="radio" name="gender" value="女性">女性{{ old('gender') }}
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">趣味</label>
+                        <label class="col-md-2" for="hobby">趣味</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="hobby">{{ old('hobby') }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">自己紹介欄</label>
+                        <label class="col-md-2" for="introduction">自己紹介欄</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="profile" rows="20">{{ old('profile') }}</textarea>
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
                     {{ csrf_field() }}
